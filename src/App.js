1ddd4a00e3d8f2d27f1654c1f5a5985897c09c30
Alec <alecinthalangsy@Alecs-MacBook-Air.local>
2018-09-07 12:21:18 -0400

    add nav component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Navbar from "./components/layout/Navbar";
+import Index from './components/layout/Index';
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
